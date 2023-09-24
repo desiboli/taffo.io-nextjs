8187685a716d1f858c39ba0168166f39b3302621
Mustafa Adam Alshammaa <adam.benals@gmail.com>
2023-09-24 11:16:08 +0200

    feat: eslint, prettier and some default components + shadcn ui

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "Next.js 13 app dir, Shadcn UI, TypeScript and Tailwind CSS Template.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+}
